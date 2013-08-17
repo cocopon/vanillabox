95e92204dd4034b415d65baa6f86e1771c01e5e5
cocopon <cocopon@me.com>
2013-08-17 23:55:32 +0900

    Add new theme

--- src/theme/compile.sh
+++ src/theme/compile.sh
@@ -5,4 +5,5 @@ STYLE=expanded
 
 cd $(dirname $0)
 
+sass bitter/bitter.scss --style $STYLE > ../../vanillabox/theme/bitter/vanillabox.css
 sass bitter_frame/bitter_frame.scss --style $STYLE > ../../vanillabox/theme/bitter_frame/vanillabox.css
