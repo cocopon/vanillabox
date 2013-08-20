e197d5028fa7e97230d3b91a2d33761687e79ca9
cocopon <cocopon@me.com>
2013-08-20 12:52:54 +0900

    Add grunt-contrib-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,20 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		qunit: {
 			all: ['test/**/*.html']
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'vanillabox/theme/bitter/vanillabox.css': 'src/theme/bitter/bitter.scss',
+					'vanillabox/theme/bitter_frame/vanillabox.css': 'src/theme/bitter_frame/bitter_frame.scss'
+				}
+			}
 		}
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 };
