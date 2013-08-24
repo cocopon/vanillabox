1fe080fcc51856188f31f148bf0423d277788626
cocopon <cocopon@me.com>
2013-08-24 12:06:40 +0900

    Add Travis CI settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,4 +49,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-jsdoc');
+
+	grunt.registerTask('test', 'qunit');
 };
